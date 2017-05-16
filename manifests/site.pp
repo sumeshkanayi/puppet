bcdac17bb3ed187071211deaa8ba089a533b3354
sumeshkanayi <sumeshkanayi@gmail.com>
2017-05-16 11:27:00 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,5 @@
+
 node 'ip-172-31-22-77.ap-southeast-1.compute.internal'{
-
-  class { '::mcollective':
-    middleware_hosts => [ 'ip-172-31-16-107' ],
-    client => true,
-
-  }
-}
-
-node 'ip-172-31-16-107.ap-southeast-1.compute.internal'{
 
  
   include mcollective
