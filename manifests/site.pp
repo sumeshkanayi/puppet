def19921319b95a78233142f6fa11854538977ee
sumeshkanayi <sumeshkanayi@gail.com>
2017-05-16 09:45:44 +0530

    Add mcollective client on puppet server

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,16 @@
+node 'ip-172-31-18-63.ap-southeast-1.compute.internal'{
+
+  class { '::mcollective':
+    middleware_hosts => [ 'ip-172-31-16-107' ],
+    client => true,
+
+  }
+}
+
 node 'ip-172-31-16-107.ap-southeast-1.compute.internal'{
 
   class { 'activemq':
+
 
 
   }
