d36b426318eb89d1fb4f6ddce2769e403f2df595
sumeshkanayi <sumeshkanayi@gmail.com>
2017-05-16 11:33:40 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,11 @@ node 'ip-172-31-22-77.ap-southeast-1.compute.internal'{
 }
 
 
+node 'ip-172-31-18-164.ap-southeast-1.compute.internal' {
+ include mcollective
+{
+
+
 node "ip-172-31-21-213.ap-southeast-1.compute.internal" {
   class{"nats": }
 }
