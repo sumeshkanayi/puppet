332280bba8c661116f421cb0d311cbbf7fe10a0c
sumeshkanayi <sumeshkanayi@gmail.com>
2017-05-20 07:47:28 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,38 +1,26 @@
 
 #client only node
-node 'ip-172-31-22-77.ap-southeast-1.compute.internal'{
+node 'ip-172-31-18-180.ap-southeast-1.compute.internal'{
 
  
   include mcollective
   
-   file{ '/tmp/test.txt': 
- 
- ensure => present,
- content => "Atlast"
- 
- }
 
 
 }
 
 
 #server (All other nodes other than clients"
-node 'ip-172-31-18-164.ap-southeast-1.compute.internal' {
+node 'ip-172-31-26-155.ap-southeast-1.compute.internal' {
  include mcollective
  
- file{ '/tmp/test.txt': 
- 
- ensure => present,
- content => "Atlast"
- 
- }
  
  
 }
 
 #puppet server
 
-node "ip-172-31-21-213.ap-southeast-1.compute.internal" {
+node "ip-172-31-18-86.ap-southeast-1.compute.internal" {
   class{"nats": }
 }
 
