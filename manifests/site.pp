31e819fbf293a088db137d64df1b93708798dc35
sumeshkanayi <sumeshkanayi@gmail.com>
2017-05-20 08:14:56 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -19,6 +19,11 @@ node 'ip-172-31-18-180.ap-southeast-1.compute.internal'{
 #server (All other nodes other than clients"
 node 'ip-172-31-26-155.ap-southeast-1.compute.internal' {
  include mcollective
+ file { '/usr/local/bin/mco':
+  
+  ensure => link,
+  target => '/opt/puppetlabs/bin/mco',
+  }
  
  
  
@@ -28,5 +33,10 @@ node 'ip-172-31-26-155.ap-southeast-1.compute.internal' {
 
 node "ip-172-31-18-86.ap-southeast-1.compute.internal" {
   class{"nats": }
+  file { '/usr/local/bin/mco':
+  
+  ensure => link,
+  target => '/opt/puppetlabs/bin/mco',
+  }
 }
 
